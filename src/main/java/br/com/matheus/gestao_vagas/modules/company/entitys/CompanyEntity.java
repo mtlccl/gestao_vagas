378b3674cec6ab0612d2b5cedd45f7ac1eabbbea
mat leme <matheus.carvalho.lemes@hotmail.com>
2025-02-11 08:18:49 -0300

    updateCompany

---
+++ src/main/java/br/com/matheus/gestao_vagas/modules/company/entitys/CompanyEntity.java
@@ -0,0 +1,39 @@
+package br.com.matheus.gestao_vagas.modules.company.entitys;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+        
+    private String name;
+    
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] nao deve conter espaço")
+    private String username;
+
+    @Email(message = "o campo (email) deve conter um email valido")
+    private String email;
+    @Length(min = 10, max = 100, message = "a senha deve conter de 10 a 100 caracteres")
+    private String password;
+    private String website;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
